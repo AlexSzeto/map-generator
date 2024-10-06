65280944865a358c549d02e73e4985f66794a76c
Alex Szeto <Alex.Szeto@gmail.com>
2024-10-06 00:04:21 -0700

    use tile index to populate tile data

--- blocks.ts
+++ blocks.ts
@@ -1,6 +1,24 @@
 //% color="#ffb236" icon="\uf0ac" block="Map Generator"
 namespace mapGen {
 
+  function getTileset(tilemap: tiles.TileMapData, imageList: Image[]): number[] {
+    const tileset = tilemap.getTileset()    
+    return imageList.map(image => {
+      const index = tileset.indexOf(image)
+      if (index === undefined) {
+        const newIndex = tileset.length
+        if (newIndex < 256) {
+          tileset.push(image)
+          return newIndex
+        } else {
+          return 0
+        }
+      } else {
+        return index
+      }
+    })
+  }
+
   //% block="generate height map terrain with tiles $tiles at scale $scale || on tilemap $tilemap"
   //% tiles.shadow="lists_create_with" tiles.defl="tileset_tile_picker"
   //% scale.defl=10
@@ -13,6 +31,7 @@ namespace mapGen {
     }
 
     const noise = new mapGen.SimplexNoise()
+    const tileset = getTileset(tilemap, tiles)
     const heightStep = 1.0 / tiles.length
 
     for (let x = 0; x < tilemap.width; x++) {
@@ -20,7 +39,7 @@ namespace mapGen {
         const height = (noise.getValue(x / scale, y / scale) + 1.0) * 0.5
         for (let tile = 0; tile < tiles.length; tile++) {
           if (height < (tile + 1) * heightStep) {
-            tilemap.setTile(x, y, tile)
+            tilemap.setTile(x, y, tileset[tile])
             break
           }
         }
